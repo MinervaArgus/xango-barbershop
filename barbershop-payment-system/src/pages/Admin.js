aa722e1d41649a6928deaaba9a9837417738ddac
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-10-18 13:05:56 -0400

    added style sheet

--- barbershop-payment-system/src/pages/Admin.js
+++ barbershop-payment-system/src/pages/Admin.js
@@ -1,6 +1,9 @@
 import React from "react";
+import '../styles/Admin.css';
 
-const Admin = () => <h1>Admin</h1>
+const Admin = () => <div>
+    <h1>Admin</h1>
+    </div>
 
 // https://v5.reactrouter.com/web/example/auth-workflow
 // https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
