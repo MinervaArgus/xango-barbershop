d8fcb384284f696fc00a98095c46c80feda8c924
SDefaultio <crismartin994@gmail.com>
2022-10-31 23:40:37 -0400

    Cleaning up Admin.js for clarity

--- barbershop-payment-system/src/pages/Admin.js
+++ barbershop-payment-system/src/pages/Admin.js
@@ -2,39 +2,29 @@ import React from "react";
 import Button from '@mui/material/Button';
 import '../styles/Admin.css';
 import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import { useState, useEffect } from 'react';
-import { storage, db } from "../firebase.js"
+import { useState } from 'react';
+import { storage } from "../firebase.js"
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import {collection, onSnapshot, query, orderBy, addDoc, serverTimestamp} from "firebase/firestore"
-
-const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'));
 
 function Admin() {
     const [imgUrl, setImgUrl] = useState(null);
     const [progresspercent, setProgresspercent] = useState(0);
-    const imagesRef = ref(storage, 'images');
-
-    const [todos, setTodos]=useState([]);
-    const [input, setInput]=useState('');
-    const [input2, setInput2]=useState('');
-
-    useEffect(() => {
-        onSnapshot(q, (snapshot) => {
-            setTodos(snapshot.docs.map(doc => ({
-                id: doc.id,
-                item: doc.data()
-            })))
-        })
-    }, [input], [input2]);
-
+    const [input, setInput] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const file = e.target[0]?.files[0]
         if (!file) return;
-        const storageRef = ref(storage, `images/${file.name}`);
+        let storageRef = ref(storage, `images/${file.name}`);
+        if (input === 1){
+            storageRef = ref(storage, `images/low-fade/${file.name}`);
+        }
+        if (input === 2){
+            storageRef = ref(storage, `images/other-cuts/${file.name}`);
+        }
+        
         const uploadTask = uploadBytesResumable(storageRef, file);
 
         uploadTask.on("state_changed",
@@ -68,11 +58,10 @@ function Admin() {
                     id="hair-style-select"
                     value={input}
                     label="Hair Style"
-                    onChange={e=>setInput(e.target.value)}
+                    onChange={() => setInput(input)}
                     >
-                    <MenuItem value={"Low Fade"}>Low-Fade</MenuItem>
-                    <MenuItem value={"Mid Fade"}>Mid-Fade</MenuItem>
-                    <MenuItem value={"Short"}>Short Hair</MenuItem>
+                    <MenuItem value={"1"}>Low-Fade</MenuItem>
+                    <MenuItem value={"2"}>Short Hair</MenuItem>
                 </Select>
                 <Button variant="contained" component="label"><input type='file' hidden/>Select File</Button>
                 <Button variant="contained" type='submit'>Upload</Button>
