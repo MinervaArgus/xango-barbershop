0b11d6396e7a18b068980ef49057038f54d6b150
SDefaultio <65252323+SDefaultio@users.noreply.github.com>
2022-11-01 13:45:21 -0400

    Removed Disfunctional Code
    Will re-add later to categorize cuts

--- barbershop-payment-system/src/pages/Admin.js
+++ barbershop-payment-system/src/pages/Admin.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import '../styles/Admin.css';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
 import { storage } from "../firebase.js"
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
@@ -11,20 +8,12 @@ import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 function Admin() {
     const [imgUrl, setImgUrl] = useState(null);
     const [progresspercent, setProgresspercent] = useState(0);
-    const [input, setInput] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const file = e.target[0]?.files[0]
         if (!file) return;
-        let storageRef = ref(storage, `images/${file.name}`);
-        if (input === 1){
-            storageRef = ref(storage, `images/low-fade/${file.name}`);
-        }
-        if (input === 2){
-            storageRef = ref(storage, `images/other-cuts/${file.name}`);
-        }
-        
+        const storageRef = ref(storage, `images/${file.name}`);
         const uploadTask = uploadBytesResumable(storageRef, file);
 
         uploadTask.on("state_changed",
@@ -48,24 +37,12 @@ function Admin() {
     return (
         <div>
             <h1>Admin</h1>
-            <h2>Image uploader</h2>
+            <h2>File uploader</h2>
             <form onSubmit={handleSubmit} className='form'>
-                <InputLabel id="hairStyleSelect">Type of Hair Style</InputLabel>
-                <Select
-                    labelId="hairStyleSelect"
-                    id="hair-style-select"
-                    value={input}
-                    label="Hair Style"
-                    onChange={() => setInput(input)}
-                    >
-                    <MenuItem value={"1"}>Low-Fade</MenuItem>
-                    <MenuItem value={"2"}>Short Hair</MenuItem>
-                </Select>
                 <Button variant="contained" component="label"><input type='file' hidden/>Select File</Button>
                 <Button variant="contained" type='submit'>Upload</Button>
-                <br></br>
             </form>
-
+            <br></br>
             {
             !imgUrl &&
                 <div className='centered-div' style={{
