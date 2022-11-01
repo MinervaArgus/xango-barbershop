5c0b9c9781ce5d8c282c4fed072c5e3196b42fdf
SDefaultio <65252323+SDefaultio@users.noreply.github.com>
2022-11-01 18:01:00 -0400

    Implementing Edit Options

--- barbershop-payment-system/src/pages/Admin.js
+++ barbershop-payment-system/src/pages/Admin.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import '../styles/Admin.css';
 import { storage, db } from "../firebase.js"
 import Service from "../components/Services";
+import ProgressBar from "../components/ProgressBar";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import { TextField, Button } from "@mui/material";
-import {collection, onSnapshot, query, addDoc, orderBy} from "firebase/firestore"
+import { TextField, Button, Select, MenuItem } from "@mui/material";
+import { doc, collection, onSnapshot, query, addDoc, orderBy, updateDoc } from "firebase/firestore"
 
 const q = query(collection(db, 'hairstylePrices'), orderBy('typeOfService'));
 
@@ -14,6 +15,9 @@ function Admin() {
     const [inputs, setInputs] = useState([]);
     const [serviceInput, setServInput] = useState('');
     const [servPriceInput, setServPriceInput] = useState(0);
+    const [dropInput, setDropInput] = useState([inputs]);
+    //const [editServInput, setEditServInput] = useState('');
+    const [editServPriceInput, setEditServPriceInput] = useState(0);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -58,13 +62,20 @@ function Admin() {
         setServPriceInput();
     };
 
+    const handleServUpdate = (e) => {
+        e.preventDefault();
+        // Reference to specific field in doc
+        const docRef = doc(db, "hairstylePrices");
+        updateDoc(docRef, {servicePrice: editServPriceInput})
+    }
+
     return (
         <div>
             <h1>Admin</h1>
             <div>
                 <h2>Image Uploader</h2>
                 <form onSubmit={handleSubmit} className='form'>
-                    <Button variant="contained" component="label"><input type='file' hidden/>Select File</Button>
+                    <Button variant="contained" component="label"><input type='file' accept='image/*' hidden/>Select File</Button>
                     <Button variant="contained" type='submit'>Upload</Button>
                 </form>
                 <br></br>
@@ -75,7 +86,8 @@ function Admin() {
                         alignItems: 'center',
                         justifyContent: 'center',
                     }}>
-                        <div className='innerbar' style={{ width: `${progresspercent}%` }}>{progresspercent}%</div>
+                        <ProgressBar  bgcolor={"#a00000"} completed={progresspercent} />;
+                        {/* <div className='innerbar' style={{ width: `${progresspercent}%` }}>{progresspercent}%</div> */}
                     </div>
                 }
                 {
@@ -89,18 +101,32 @@ function Admin() {
                 <form className="form">
                     <TextField id="outlined-basic" label="Service Name" variant="outlined" value={serviceInput} InputLabelProps={{shrink: true}} onChange={e => setServInput(e.target.value)}/>
                     <TextField id="outlined-basic" label="Service Price" variant="outlined" value={servPriceInput} InputLabelProps={{shrink: true}} onChange={e => setServPriceInput(e.target.value)}/>
-                    <Button variant="contained" onClick={handleServSubmit}>Add Service</Button>
+                    <Button variant="contained" onClick={handleServSubmit}>Edit Service</Button>
                 </form>
+            </div>
 
+            <br></br>
+
+            <div>
+                <h2>Edit Service</h2>
+                <form>
+                    <Select id="outlined-basic" label="Service Name" variant="outlined" value={dropInput} InputLabelProps={{shrink: false}} onChange={e => setDropInput(e.target.value)}>
+                        {/* <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem> */}
+                    </Select>
+                    <TextField id="outlined-basic" label="Service Price" variant="outlined" value={editServPriceInput} InputLabelProps={{shrink: true}} onChange={e => setEditServPriceInput(e.target.value)}/>
+                    <Button variant="contained" type="submit" onClick={handleServUpdate}>Edit Service</Button>
+                </form>
+            </div>
+
+            <br></br>
+
+            <div>                
                 <h2>List of Services and Prices</h2>
                 <ul>
                     {inputs.map(item => <Service key = {item.id} arr = {item}/>)}
                 </ul>
-                
-                {/* <h2>Edit Service</h2>
-                <Button variant="contained" type="submit">Edit Service</Button> */}
-                
-
             </div>
         </div>
     );
