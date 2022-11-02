9eecda2ec730e01df19b7f32fe4d5625993fc7c0
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-11-02 15:27:41 -0400

    Added ImageUploader.js as a component

--- barbershop-payment-system/src/firebase.js
+++ barbershop-payment-system/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import "firebase/storage";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore';
 
