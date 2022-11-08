45c1a11ef06e4e272e5744fe28bd4b09942367d1
SDefaultio <crismartin994@gmail.com>
2022-11-08 13:50:58 -0500

    Update Firebase.js

--- barbershop-payment-system/src/Firebase.js
+++ barbershop-payment-system/src/Firebase.js
@@ -1,24 +1,37 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import "firebase/storage";
+import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCGRSnrD_xkU1V7wIuaoA_WsSOhAvwzICk",
-    authDomain: "bsp-project-e1b90.firebaseapp.com",
-    projectId: "bsp-project-e1b90",
-    storageBucket: "bsp-project-e1b90.appspot.com",
-    messagingSenderId: "600531881070",
-    appId: "1:600531881070:web:b4da5d05cd9308f03e2a7f",
-    measurementId: "G-X5SN3D1RP2"
+  apiKey: "AIzaSyCGRSnrD_xkU1V7wIuaoA_WsSOhAvwzICk",
+  authDomain: "bsp-project-e1b90.firebaseapp.com",
+  projectId: "bsp-project-e1b90",
+  storageBucket: "bsp-project-e1b90.appspot.com",
+  messagingSenderId: "600531881070",
+  appId: "1:600531881070:web:b4da5d05cd9308f03e2a7f",
+  measurementId: "G-X5SN3D1RP2"
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 const db = getFirestore(app);
-export { db }
+
+export {auth, storage, db, logInWithEmailAndPassword, logout}
