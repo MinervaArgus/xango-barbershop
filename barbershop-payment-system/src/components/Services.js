111dfd61c1e7e3cce1eaa9a44d145abb0f2dedab
SDefaultio <crismartin994@gmail.com>
2023-02-09 18:45:10 -0500

    Update Services.js

--- barbershop-payment-system/src/components/Services.js
+++ barbershop-payment-system/src/components/Services.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from '../Firebase';
-import { Card, Container, Form, InputGroup, Button, ButtonGroup, Modal } from "react-bootstrap"; 
+import { Card, Container, Form, InputGroup, Button, ButtonGroup, Toast, ToastContainer, Modal } from "react-bootstrap"; 
 
 const Service = ({arr}) => {
   const numberFormat = (value) => new Intl.NumberFormat('en-IN', {style: 'currency', currency: 'EUR'}).format(value);
 
   const [editServNameInput, setEditServNameInput] = useState("");
   const [editServPriceInput, setEditServPriceInput] = useState("");
+  const [showEditNotif, setShowEditNotif] = useState(false);
   const [open, setOpen] = useState(false);
 
   const updateFunc = (e) => {
@@ -32,17 +33,14 @@ const Service = ({arr}) => {
       }
     }
 
+    setShowEditNotif(true);
     setEditServNameInput("");
     setEditServPriceInput("");
   }
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  }
+  const handleClickOpen = () => setOpen(true);
 
-  const handleClose = () => {
-    setOpen(false);
-  }
+  const handleClose = () => setOpen(false);
 
   const confirmRemoval = () => {
     deleteDoc(doc(db, "hairstylePrices", arr.id))
@@ -50,67 +48,72 @@ const Service = ({arr}) => {
 
   return (
       <Container className="mt-4 mb-2 justify-content-md-center">
-          <Card style={{ width: '19rem' }}>
-            <Card.Body>
-              <Card.Title>{arr.item.typeOfService}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Precio de Servicio: {numberFormat(arr.item.servicePrice)}</Card.Subtitle>
+        {
+          showEditNotif
+          ? 
+          (<ToastContainer className="p-3" containerPosition="fixed" position="top-end">
+              <Toast onClose={() => setShowEditNotif(false)} show={showEditNotif} delay={5000} autohide>
+                  <Toast.Header>
+                      <strong className="me-auto">Atención!</strong>
+                  </Toast.Header>
+                  <Toast.Body>Servicio cambiado!</Toast.Body>
+              </Toast>
+          </ToastContainer>) 
+          : 
+          null
+        }
 
-              <Form>
-                  <InputGroup className="mb-3">
-                      <Form.Control 
-                          type="input" 
-                          name='Service Name' 
-                          value={editServNameInput}
-                          placeholder={"Nuevo nombre de Servicio"} 
-                          onChange={e => setEditServNameInput(e.target.value)}
-                      />
-                  </InputGroup>
-                  
-                  <InputGroup className="mb-3">
-                      <InputGroup.Text>€</InputGroup.Text>
-                      <Form.Control 
-                          type="input" 
-                          name='Service Price' 
-                          value={editServPriceInput}
-                          placeholder={"00.00"} 
-                          onChange={e => setEditServPriceInput(e.target.value)}
-                      />
-                  </InputGroup>
-              </Form>
+
+        <Card style={{ width: '19rem' }}>
+          <Card.Body>
+            <Card.Title>{arr.item.typeOfService}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Precio de Servicio: {numberFormat(arr.item.servicePrice)}</Card.Subtitle>
+
+            <Form>
+                <InputGroup className="mb-3">
+                    <Form.Control 
+                        type="input" 
+                        name='Service Name' 
+                        value={editServNameInput}
+                        placeholder={"Nuevo nombre de Servicio"} 
+                        onChange={e => setEditServNameInput(e.target.value)}
+                    />
+                </InputGroup>
                 
-                <ButtonGroup>
-                    <Button variant="primary" onClick={updateFunc}>Cambiar Servicio</Button> {' '}
-                    <Button variant="danger" onClick={handleClickOpen}>Eliminar Servicio</Button>
-                </ButtonGroup>
-            </Card.Body>
-          </Card>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>€</InputGroup.Text>
+                    <Form.Control 
+                        type="input" 
+                        name='Service Price' 
+                        value={editServPriceInput}
+                        placeholder={"00.00"} 
+                        onChange={e => setEditServPriceInput(e.target.value)}
+                    />
+                </InputGroup>
+            </Form>
+              
+              <ButtonGroup>
+                  <Button variant="primary" onClick={updateFunc}>Cambiar Servicio</Button> {' '}
+                  <Button variant="danger" onClick={handleClickOpen}>Eliminar Servicio</Button>
+              </ButtonGroup>
+          </Card.Body>
+        </Card>
+
+        <Modal show={open} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Advertencia!</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>¿Está seguro que quieres borrar este servicio?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+              No
+            </Button>
+            <Button variant="danger" onClick={confirmRemoval}>
+              Borrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
-
-
-    // Use <Modal>
-
-    //     <Dialog
-    //       open={open}
-    //       onClose={handleClose}
-    //       aria-labelledby="alert-dialog-title"
-    //       aria-describedby="alert-dialog-description"
-    //     >
-    //       <DialogTitle id="alert-dialog-title">
-    //         {"Are you sure you want to remove: "}
-    //       </DialogTitle>
-    //       <DialogContent>
-    //         <DialogContentText id="alert-dialog-description">
-    //           You will have to manually add <b>"{arr.item.typeOfService}"</b> and it's 
-    //           price: <b>"{numberFormat(arr.item.servicePrice)}"</b> if you want to undo your changes.
-    //         </DialogContentText>
-    //       </DialogContent>
-    //       <DialogActions>
-    //         <Button onClick={handleClose}>Go Back</Button>
-    //         <Button onClick={confirmRemoval} autoFocus>
-    //           Confirm
-    //         </Button>
-    //       </DialogActions>
-    //     </Dialog>
   )
 }
 
