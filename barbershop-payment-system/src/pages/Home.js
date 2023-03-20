884c66030acad4f1089901d75cf985e58d61ee2f
SDefaultio <crismartin994@gmail.com>
2023-03-20 17:24:11 -0400

    Added Tailwind CSS

--- barbershop-payment-system/src/pages/Home.js
+++ barbershop-payment-system/src/pages/Home.js
@@ -2,6 +2,6 @@ import React from "react";
 import { Container } from "react-bootstrap";
 // import '../styles/Home.css';
 
-const Home = () => <Container className="my-2"><h1>Home</h1></Container>
+const Home = () => <Container className="my-2"><h1 className="tw-text-3xl tw-font-bold tw-underline">Home</h1></Container>
 
 export default Home
