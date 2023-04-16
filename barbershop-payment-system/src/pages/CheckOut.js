b219159a3b6bf3543736d45bdedc10e962e4712e
jincera <jincera@radford.edu>
2023-04-16 08:46:46 -0400

    fixing checkout p3

--- barbershop-payment-system/src/pages/CheckOut.js
+++ barbershop-payment-system/src/pages/CheckOut.js
@@ -7,28 +7,37 @@ import { Container, Button } from 'react-bootstrap'
 
 export default function CheckOut() {
     const [showItem, setShowItem] = useState(false)
+    const [isAppointment, setIsAppointment] = useState(false);
     const location = useLocation()
     const { appointment, date, time, appointmentID, amount, cart } = location.state
     let h3;
     let stripeContatiner;
+    function appointmentCase() {
+        setShowItem(true);
+        setIsAppointment(true);
+    }
+    function productsCase() {
+        setShowItem(true);
+        setIsAppointment(false);
+    }
     if (appointment) {
         h3 = <><h3>Total amount: ${appointment.price}</h3>
-            <Button id="StripeButton" onClick={() => setShowItem(true)}>Purchase</Button>
+            <Button id="StripeButton" onClick={() => appointmentCase()}>Purchase</Button>
         </>
         console.log("inside appointment");
-        stripeContatiner = <><StripeContainer
+        /* stripeContatiner = <><StripeContainer
             appointment={appointment}
             date={date}
             time={time}
-            id={appointmentID} /></>
+            id={appointmentID} /></> */
 
     } else if (amount) {
         h3 = <><h3>Total amount: ${amount}</h3>
-            <Button id="StripeButton" onClick={() => setShowItem(true)}>Purchase</Button>
+            <Button id="StripeButton" onClick={() => productsCase()}>Purchase</Button>
         </>
         console.log("inside amount");
-        stripeContatiner = <><StripeContainer
-            products={cart} /></>
+        /* stripeContatiner = <><StripeContainer
+            products={cart} /></> */
     } else {
         console.log("inside error");
         h3 = <h3>Error</h3>
@@ -38,8 +47,18 @@ export default function CheckOut() {
         // <<<<<<< Updated upstream
         <Container className='my-4'>
             {showItem
-                ?
-                <>{stripeContatiner}</>
+                ? <>
+                    {isAppointment
+                        ?
+                        <StripeContainer
+                            appointment={appointment}
+                            date={date}
+                            time={time}
+                            id={appointmentID} />
+                        :
+                        <StripeContainer
+                            products={cart} />
+                    }</>
                 :
                 <> {h3}</>
             }
